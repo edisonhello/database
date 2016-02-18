1fef31a4a20a10e08f79fec16ca7448befff3c0c
edisonhello <edisonhello@hotmail.com>
2016-02-18 16:20:53 +0800

    add add method

--- app.js
+++ app.js
@@ -19,17 +19,31 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', function(req,res){
 	res.sendFile(__dirname+'/index.html', function(){res.end();})
 })
+app.get('/add.html', function(req,res){
+	res.sendFile(__dirname+'/add.html', function(){res.end();})
+})
 
 io.sockets.on('connection', function(socket){
-	console.log("asd");
 	socket.on('reqall', function(){
 		MongoClient.connect('mongodb://localhost:27017/datbs',function(err, db){
 			db.collection('datbs').find({},function(a,b){
 				b.toArray(function(err, callback){
-					console.log(callback);
-					socket.emit('allisthis', callback);
-				});
+					var str = JSON.stringify(callback);
+					str = str.split("},{");
+					str[0] = str[0].slice(2);
+					str[str.length-1] = str[str.length-1].slice(0,str[str.length-1].length-2)
+					str = str.toString()
+					str = str.replace(/","_/g,'"<br>"_')
+					console.log(str);
+					socket.emit('allisthis', str);
+				})
 			})
+		})
+	})
+
+	socket.on('additem', function(title, descr, tag){
+		MongoClient.connect('mongodb://localhost:27017/datbs',function(err,db){
+			db.collection('datbs').insert({"title": title, "descr": descr, "tag": tag});
 		})
 	})
 })
