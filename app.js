d391bd4e7c6e5a61cb3699db70ab42741f0d49ef
edisonhello <edisonhello@hotmail.com>
2016-03-06 18:47:11 +0800

    TDC and def cons

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ setInterval(function(){
 }, 1000)
 
 var count;
-MongoClient.connect('mongodb://localhost:27017/datbs', function(err, db){
+MongoClient.connect('mongodb://188.166.216.86:27017/datbs', function(err, db){
 	db.collection('counting').find({}).count(function(err,cnt){
 		count = cnt;
 	})
@@ -64,7 +64,7 @@ MongoClient.connect('mongodb://localhost:27017/datbs', function(err, db){
 
 io.sockets.on('connection', function(socket){
 	socket.on('reqall', function(){
-		MongoClient.connect('mongodb://localhost:27017/datbs', function(err, db){
+		MongoClient.connect('mongodb://188.166.216.86:27017/datbs', function(err, db){
 			db.collection('datbs').find({},{_id: 0, creater: 0}, function(err,result){
 				result.toArray(function(err, callback){
 					var str = JSON.stringify(callback);
@@ -81,7 +81,7 @@ io.sockets.on('connection', function(socket){
 	})
 
 	socket.on('additem', function(title, descr, tag, creater){
-		MongoClient.connect('mongodb://localhost:27017/datbs',function(err,db){
+		MongoClient.connect('mongodb://188.166.216.86:27017/datbs',function(err,db){
 			count++;
 			db.collection('datbs').insert({"id": count.toString(), "time": now, "title": title, "descr": descr, "tag": tag, "creater": creater});
 			db.collection('counting').insert({});
@@ -89,7 +89,7 @@ io.sockets.on('connection', function(socket){
 	})
 
 	socket.on('findreq', function(findType, findThing){
-		MongoClient.connect('mongodb://localhost:27017/datbs', function(err,db){
+		MongoClient.connect('mongodb://188.166.216.86:27017/datbs', function(err,db){
 			if(findType == "title"){
 				db.collection('datbs').find({"title":findThing},{_id: 0, creater: 0}, function(err,result){
 					result.toArray(function(err, callback){
@@ -116,11 +116,24 @@ io.sockets.on('connection', function(socket){
 					})
 				})
 			}
+			if(findType == "creater"){
+				db.collection('datbs').find({"creater": findThing},{_id: 0, creater: 0}, function(err,result){
+					result.toArray(function(err, callback){
+						var str = JSON.stringify(callback);
+						str = str.split("},{");
+						str[0] = str[0].slice(2);
+						str[str.length-1] = str[str.length-1].slice(0,str[str.length-1].length-2);
+						str = str.toString();
+						str = str.replace(/],"i/g,']<br>"');
+						socket.emit('allisthis', str);
+					})
+				})
+			}
 		})
 	})
 
 	socket.on('thisiddetail?', function(id){
-		MongoClient.connect('mongodb://localhost:27017/datbs', function(err, db){
+		MongoClient.connect('mongodb://188.166.216.86:27017/datbs', function(err, db){
 			db.collection('datbs').find({"id": id}, function(err, result){
 				db.collection('datbs').find({"id": id}).count(function(err, cnt){
 					if(cnt){
@@ -136,20 +149,34 @@ io.sockets.on('connection', function(socket){
 		})
 	})
 
-	socket.on('updateitem', function(title, descr, tag, thisid){
-		MongoClient.connect('mongodb://localhost:27017/datbs',function(err,db){
-			db.collection('datbs').update({"id":thisid}, {"id": thisid, "time": now, "title": title, "descr": descr, "tag": tag});
+	socket.on('updateitem', function(title, descr, tag, thisid, thispw){
+		MongoClient.connect('mongodb://188.166.216.86:27017/datbs',function(err,db){
+			db.collection('users').find("username":thisid , "pass":thispw).count(function(err, cnt){
+				if(cnt == 0){
+					socket.emit('noperedit');
+				}
+				else{
+					db.collection('datbs').update({"id":thisid}, {"id": thisid, "time": now, "title": title, "descr": descr, "tag": tag});
+				}
+			})
 		})
 	})
 
-	socket.on('deletethis', function(thisid){
-		MongoClient.connect('mongodb://localhost:27017/datbs',function(err,db){
-			db.collection('datbs').remove({"id": thisid.toString()});
+	socket.on('deletethis', function(thisid, thisid, thispw){
+		MongoClient.connect('mongodb://188.166.216.86:27017/datbs',function(err,db){
+			db.collection('users').find("username":thisid , "pass":thispw).count(function(err, cnt){
+				if(cnt == 0){
+					socket.emit('noperedit');
+				}
+				else{
+					db.collection('datbs').remove({"id": thisid.toString()});
+				}
+			})
 		})
 	})
 
 	socket.on('regreq', function(username, pass){
-		MongoClient.connect('mongodb://localhost:27017/datbs', function(err,db){
+		MongoClient.connect('mongodb://188.166.216.86:27017/datbs', function(err,db){
 			db.collection('users').find({"username": username}).count(function(err, cnt){
 				if(cnt){
 					socket.emit('username2');
@@ -163,10 +190,10 @@ io.sockets.on('connection', function(socket){
 	})
 
 	socket.on('loginreq', function(username, pass){
-		MongoClient.connect('mongodb://localhost:27017/datbs', function(err,db){
+		MongoClient.connect('mongodb://188.166.216.86:27017/datbs', function(err,db){
 			db.collection('users').find({"username": username, "pass": pass}).count(function(err, cnt){
 				if(cnt){
-					socket.emit('logindone', username);
+					socket.emit('logindone', username, pass);
 				}
 				else{
 					socket.emit('loginfail');
@@ -179,4 +206,4 @@ io.sockets.on('connection', function(socket){
 
 
 
-server.listen(8080);
+server.listen(7122);
